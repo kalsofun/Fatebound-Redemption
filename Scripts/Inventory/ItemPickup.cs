172959d85c258cfdd504debae7abb523afa6370b
Kalso <kalsofun@gmail.com>
2026-02-10 16:53:35 +0800

    test

--- Scripts/Inventory/ItemPickup.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class ItemPickup : MonoBehaviour
-{
-    public ItemData itemData;
-    public bool isPickedUp = false;
-
-    void Awake() => itemData.ID = gameObject.name;
-}
